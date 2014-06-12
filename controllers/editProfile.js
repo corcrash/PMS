f47225e3e5f3e798db3638876245558ea1986acb
AlexBenic <aleksandar.benic@hotmail.com>
2014-06-12 21:33:38 +0200

    few typo's in editProfile.js

--- controllers/editProfile.js
+++ controllers/editProfile.js
@@ -1,9 +1,9 @@
 /**
  * Created by aleksandar on 12.6.14..
  */
-var mysql = reqire ("../config/database");
+var mysql = require ("../config/database");
 
-module.export = function (req,res){
+module.exports = function (req,res){
 
     var id = req.user.id;
     if(id = undefined) {
@@ -17,33 +17,32 @@ module.export = function (req,res){
             return;
         }
         //console.log(id);
-        if (!(req.body.userInfo = undefined))
-        {
+        if (!(req.body.userInfo = undefined)) {
             var user_disp = req.body.userInfo.displayName;
             var user_avatar = req.body.userInfo.avatar;
             var user_desc = req.body.userInfo.description;
 
             connection.query("INSERT INTO pms ( pms.avatar, pms.description, pms.display_name )" +
-                " VALUES (?,?,?)", [user_avatar,user_desc,user_disc], function (err, result) {
+                " VALUES (?,?,?)", [user_avatar, user_desc, user_disc], function (err, result) {
                 if (err) {
                     console.error(err);
                     return;
                 }
 
-                var status={
-                    status : true,
+                var status = {
+                    status: true,
                     message: "insert_into_db_successful"
                 };
 
                 res.send(status);
             })
         }
-        else
-        {
-            var status={
+        else {
+            var status = {
                 status: false,
                 message: "package_not_recieved"
             }
         }
+    })
 
-}
+};
