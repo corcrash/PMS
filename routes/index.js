b0b3966e9a2c78edee6d72d366ca3bcd6f2665e2
milanche911 <milan.dicic@gmail.com>
2014-06-13 16:46:12 +0200

    error reporting za login

--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,30 @@ var getProjects = require('../controllers/getProject');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('login', { title: 'Express' });
+  res.render('login', { message: req.flash('loginMessage') });
 });
 
-router.route('/').get(function(req,res){
-    res.render('profile');//za sada
+router.route('/index').get(function(req,res){
+    res.render('index');
 });
+
+router.route('/login').get(function(req,res){
+    res.render('login');
+});
+
 router.route('/signup').post(passport.authenticate('local-signup', {
     successRedirect : '/', // redirect to the secure profile section
     failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
 }))
     .get(function(req, res){
-        res.send("signup");
+        //res.send("signup");
+        res.render('signup');
     });
 
 router.route('/login').post(passport.authenticate('local-login', {
-    successRedirect : '/', // redirect to the secure profile section
-    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    successRedirect : '/index', // redirect to the secure profile section
+    failureRedirect : '/', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
 }))
     .get(function(req, res){
